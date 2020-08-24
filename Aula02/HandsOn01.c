9d8ff99a3d5c43aee1c376786c78dd2d669e163f
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2020-08-24 16:11:47 -0300

    Create HandsOn01.c

---
+++ Aula02/HandsOn01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("%d\n",sizeof(int));//Imprime tamanho de um inteiro
+	printf("%d\n",sizeof(char));//Imprime tamanho de um char
+	printf("%d\n",sizeof(float));//Imprime tamanho de um float
+	printf("%d\n",sizeof(double));//Imprime tamanho de um double
+	printf("%d\n",sizeof(int*));//Imprime tamanho de um ponteiro de inteiro
+	return 0;
+}
